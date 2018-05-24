7de23d0aaff46b8dfffa7807685ac264ef80d5c4
Ben Truong <bhtruon1@uci.edu>
2018-05-24 15:16:59 -0700

    Added gaussian blurring

--- src/webcam.py
+++ src/webcam.py
@@ -2,21 +2,18 @@
 Simply display the contents of the webcam with optional mirroring using OpenCV 
 via the new Pythonic cv2 interface.  Press <esc> to quit.
 """
-
+import time
 import cv2
 import numpy as np
 from PIL import Image
-width = 32
-
 
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
 def formatImage(img):
     img = rgb2gray(img)
-#    wpercent = width/float(img.shape[0])
-#    hsize = int(float(img.shape[1])*float(wpercent))
-    img = cv2.resize(img, dsize=(64, 64), interpolation=cv2.INTER_CUBIC)
+    img = cv2.resize(img, dsize=(128, 128), interpolation=cv2.INTER_CUBIC)
+    img = cv2.GaussianBlur(img, (5, 5), 0)
     cv2.imwrite("image.jpeg", img)
     return img     
 
@@ -24,6 +21,7 @@ def show_webcam(mirror=False):
     cam = cv2.VideoCapture(0)
     l = np.zeros(10)
     while True:
+        time.sleep(0.05)
         #ret_val is a boolan statement indicating that the value has been returned
         ret_val, img = cam.read()
         if mirror: 
@@ -34,7 +32,6 @@ def show_webcam(mirror=False):
         if cv2.waitKey(1) != -1: 
             break  # esc to quit
     cv2.destroyAllWindows()
-
 
 def main():
     show_webcam(mirror=True)
