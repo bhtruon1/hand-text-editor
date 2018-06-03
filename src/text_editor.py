bbfe2d9235e4ebaaa323acf1cfc7f91cbcf28028
Sui Feng Xu <sfxu@uci.edu>
2018-06-02 19:43:46 -0700

    changed text_editor

---
+++ src/text_editor.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+class TextEditor:
+    def __init__(self):
+        self.ROOT=Tk("Text Editor")
+        self.ROOT.title("Hand Gesture Text Editor")
+        self.CONTAINER=Text(self.ROOT)
+        self.CONTAINER.insert(END, "")
+        self.CONTAINER.grid()
+        self.ROOT.update()
+
+    def insert(self, letter):
+        self.CONTAINER.insert(END, letter)
+        self.ROOT.update()
+
+    def delete(self):
+        self.CONTAINER.delete("end-2c")
+        self.ROOT.update()
