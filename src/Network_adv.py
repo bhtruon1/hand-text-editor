56ed9e88bae815c99f66a3fae7285b5628ba405d
cmo3 <cmo3@uci.edu>
2018-06-14 12:23:29 -0700

    final model and changes

--- src/Network_adv.py
+++ src/Network_adv.py
@@ -7,40 +7,47 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.num_channels = 4  # output channel for the first layer
         self.dropout_rate = 0.8
 
-        # 32*32
+        # 64*64
         self.conv1 = nn.Conv2d(3, 4, 3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(4)
 
-        # 16*16
+        # 32*32
         self.conv2 = nn.Conv2d(4, 8, 3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(8)
 
-        # 8*8
+        # 16*16
         self.conv3 = nn.Conv2d(8, 16, 3, stride=1, padding=1)
         self.bn3 = nn.BatchNorm2d(16)
 
+        # 8*8
+        self.conv4 = nn.Conv2d(16, 32, 3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(32)
+
         # 4*4
-        self.fc1 = nn.Linear(4 * 4 * 16, 128)
-        self.fcbn1 = nn.BatchNorm1d(128)
-        self.fc2 = nn.Linear(128, 38)
+        self.fc1 = nn.Linear(4 * 4 * 32, 1024)
+        self.fcbn1 = nn.BatchNorm1d(1024)
+        self.fc2 = nn.Linear(1024, 38)
 
-    def forward(self, s):
+        self.pool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
         # size 3 x 64 x 64
-        s = self.bn1(self.conv1(s))  # batch_size x num_channels x 64 x 64
-        s = F.relu(F.max_pool2d(s, 2))  # batch_size x num_channels x 32 x 32
-        s = self.bn2(self.conv2(s))  # batch_size x num_channels*2 x 32 x 32
-        s = F.relu(F.max_pool2d(s, 2))  # batch_size x num_channels*2 x 16 x 16
-        s = self.bn3(self.conv3(s))  # batch_size x num_channels*4 x 16 x 16
-        s = F.relu(F.max_pool2d(s, 2))  # batch_size x num_channels*4 x 8 x 8
-
+        x = self.bn1(self.conv1(x))  # batch_size x num_channels x 64 x 64
+        x = F.relu(self.pool(x))  # batch_size x num_channels x 32 x 32
+        x = self.bn2(self.conv2(x))  # batch_size x num_channels*2 x 32 x 32
+        x = F.relu(self.pool(x))  # batch_size x num_channels*2 x 16 x 16
+        x = self.bn3(self.conv3(x))  # batch_size x num_channels*4 x 16 x 16
+        x = F.relu(self.pool(x))  # batch_size x num_channels*4 x 8 x 8
+        x = self.bn4(self.conv4(x))
+        x = F.relu(self.pool(x))
         # Flatten
-        s = s.view(-1, 4 * 4 * 16)  # batch_size x 8*8*num_channels*4
+        x = x.view(-1, 4 * 4 * 32)  # batch_size x 8*8*num_channels*4
 
-        s = F.dropout(F.relu(self.fcbn1(self.fc1(s))),
-                      p=self.dropout_rate, training=self.training)  # batch_size x self.num_channels*4
-        s = self.fc2(s)  # batch_size x 6
+        #x = F.dropout(F.relu(self.fcbn1(self.fc1(x))),
+                      #p=self.dropout_rate, training=self.training)  # batch_size x self.num_channels*4
+        x = self.fc1(x)
+        x = self.fc2(x)  # batch_size x 6
 
-        return F.log_softmax(s, dim=1)
+        return F.log_softmax(x, dim=1)
---
+++ src/train.py
@@ -0,0 +1,133 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as dset
+import Network_adv
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import pandas as pd
+from torch.utils.data.sampler import SubsetRandomSampler
+import torch.optim as optim
+from torch.autograd import Variable
+import time
+import matplotlib.pyplot as plt
+# from setGPU import *
+import os
+
+num_epochs = 5
+LOSS = []
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        N, C, H, W = x.size() # read in N, C, H, W
+        return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
+
+
+def createLossAndOptimizer(net, learning_rate=0.001):
+    # Loss function
+    loss = torch.nn.CrossEntropyLoss()
+
+    # Optimizer
+    optimizer = optim.Adam(net.parameters(), lr=learning_rate)
+
+    return loss, optimizer
+
+def train(net, criterion, optimizer):
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            # get the inputs
+            inputs, labels = data
+            #inputs_var = Variable(inputs.type(dtype))
+            #labels_var = Variable(labels.type(dtype).long())
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            # with cuda
+            # outputs = net(inputs_var)
+            # loss = criterion(outputs, labels_var)
+
+            # without cuda
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            LOSS.append(loss.item())
+            if i % 50 == 49:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss/50))
+                running_loss = 0.0
+    print('Finished Training')
+
+def accuracy(net, test_loader):
+    dataiter = iter(test_loader)
+    images, labels = dataiter.next()
+
+
+    correct = 0
+    total = 0
+    #class_correct = list(0. for i in range(24))
+    #class_total = list(0. for i in range(24))
+    class_correct = list(0. for i in range(38))
+    class_total = list(0. for i in range(38))
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            #images_var = Variable(images.type(dtype))
+            #labels_var = Variable(labels.type(dtype).long())
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+    print('Accuracy of the network on test images: %d %%' % (100 * correct / total))
+
+    for i in range(38):
+        if class_total[i] == 0:
+            print('Accuracy of %5s : %2d %%' % (classes[i], 0))
+        else:
+            print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
+trainset = dset.ImageFolder(root='edge/training', transform=transform)
+# trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+testset = dset.ImageFolder(root='edge/testing', transform=transform)
+# testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+
+#classes = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I','K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y')
+classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+           'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+           'U', 'V', 'W', 'X', 'Y', 'Z', 'BS', 'SP')
+
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=True, num_workers=2)
+test_loader = torch.utils.data.DataLoader(testset, batch_size=16, shuffle=False, num_workers=2)
+net = Network_adv.Net()
+
+# commented out for cpu
+# net, dtype = gpu(net)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.0001, momentum=0.9)
+
+train(net, criterion, optimizer)
+accuracy(net, test_loader)
+# save_model(net)
+
+
+torch.save(net, 'checkpoint.pth.tar')
+
+#last_json_path = os.path.join(model_dir, "metrics_val_last_weights.json")
+#torch.utils.save_dict_to_json(val_metrics, last_json_path)
+print(LOSS)
